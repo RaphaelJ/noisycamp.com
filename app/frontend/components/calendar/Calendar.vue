f9f258a31df7eac1b0782677f05c079ffeabd683
Raphael Javaux <raphaeljavaux@gmail.com>
2019-10-04 12:47:37 +0200

    Basic support for events in Vue Calendar.

--- app/frontend/components/calendar/Calendar.vue
+++ app/frontend/components/calendar/Calendar.vue
@@ -25,10 +25,14 @@
                 <div class="day-label"></div>
                 <div
                     class="day-label"
-                    v-for="abbr in dayAbbrs"
-                    :key="abbr">
-                    <div class="day-label-name">{{ abbr }}</div>
-                    <small class="day-label-date">Oct 23</small>
+                    v-for="day in 7"
+                    :key="day">
+                    <div class="day-label-name">
+                        {{ weekDays[day - 1].format("ddd") }}
+                    </div>
+                    <small class="day-label-date">
+                        {{ weekDays[day - 1].format("MMM D") }}
+                    </small>
                 </div>
             </div>
 
@@ -37,21 +41,33 @@
                     <div class="hour-labels">
                         <div
                             class="hour-label"
-                            v-for="index in 24"
-                            :key="index">
-                            <small>{{ index - 1 }}:00</small>
+                            v-for="hour in 24"
+                            :key="hour">
+                            <small>{{ hour - 1 }}:00</small>
                         </div>
                     </div>
 
                     <div
                         class="day"
-                        v-for="abbr in dayAbbrs"
-                        :key="abbr">
+                        v-for="day in 7"
+                        :key="day">
 
                         <div
                             class="hour"
-                            v-for="index in 24"
-                            :key="index">
+                            v-for="hour in 24"
+                            :key="hour">
+                        </div>
+                    </div>
+
+                    <div
+                        v-for="(event, index) in weekEvents"
+                        :key="'event-' + index"
+                        class="event-container">
+                        <div
+                            v-for="(style, styleIndex) in eventStyles(event)"
+                            :key="'event-' + index + '-style-' + styleIndex"
+                            :class="eventClasses(event)"
+                            :style="style">
                         </div>
                     </div>
                 </div>
@@ -61,22 +77,120 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import Vue, { PropOptions } from "vue";
+
+import * as moment from 'moment';
 
 declare var NC_CONFIG: any;
 
 export default Vue.extend({
     props: {
-        timezone: { type: String, required: true },
+        // The current calendar time, without timezone.
+        currentTime: { type: String, required: true },
+
+        // The list of events to be displayed in the calendar, with calendar
+        // local dates as ISO 8601 strings.
+        events: <PropOptions<Object[]>>{ type: Array, required: true },
+
+        // The CSS classes of the event.
+        classes: <PropOptions<Object[]>>{ type: Array, default: () => [] },
     },
     data() {
-        return {
-            dayAbbrs: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-        }
+        return { }
+    },
+    computed: {
+        // Current time as a MomentJS object.
+        mCurrentTime() {
+            return moment(this.currentTime)
+        },
+
+        // Calendar events with MomentJS objects.
+        mEvents() {
+            return this.events
+                .map(event => {
+                    return {
+                        startsAt: moment(event['starts-at']),
+                        endsAt: moment(event['ends-at']),
+                        classes: event['classes']
+                    }
+                });
+        },
+
+        // Returns the date (at 00:00) of the current week's Monday.
+        weekStart() {
+            return this.mCurrentTime.startOf('isoWeek');
+        },
+
+        // Returns day dates (at 00:00) of the current week.
+        weekDays() {
+            let dates = Array(7);
+            for (var i = 0; i < 7; ++i) {
+                dates[i] = this.weekStart.clone().add(i, 'days');
+            }
+            return dates;
+        },
+
+        // Events of the currently shown week.
+        weekEvents() {
+            let monday = this.weekStart;
+            let nextMonday = monday.clone().add(7, 'days');
+
+            return this.mEvents
+                .filter(event => {
+                    return event.startsAt >= monday && event.startsAt < nextMonday;
+                });
+        },
     },
     methods: {
-        hoursInDay(day) {
-            return 24;
+        eventClasses(event) {
+            return ['event'].concat(event.classes);
+        },
+
+        // Returns a list of CSS positioning parameters for event (one for each day the event
+        // applies).
+        eventStyles(event) {
+            // Converts the time part of a date+time value to a decimal value in
+            // [0..24].
+            function timeDecimal(datetime) {
+                return datetime.hour()
+                    + datetime.minute() / 60.0
+                    + datetime.second() / 3600.0;
+            }
+
+            let startDay = event.startsAt.clone().startOf('day');
+            let endsDay = event.endsAt.clone().startOf('day');
+
+            // Loops over the days the event is applied on.
+            let nDays = endsDay.diff(startDay, 'days') + 1;
+            let styles = Array(nDays);
+            for (var i = 0; i < nDays; ++i) {
+                let day = startDay.clone().add(i, 'days');
+
+                // Does the event starts during the day?
+                let startTimeDec =
+                    i == 0
+                    ? timeDecimal(event.startsAt)
+                    : 0;
+
+                // Does the event stops during the day?
+                let endTimeDec =
+                    i == nDays - 1
+                    ?  timeDecimal(event.endsAt)
+                    : 24;
+
+                console.log(day);
+                console.log(startTimeDec);
+                console.log(endTimeDec);
+                console.log();
+
+                styles[i] = {
+                    top: `calc(${startTimeDec} / 24 * 100%)`,
+                    left: `calc(${day.isoWeekday()} / 8 * 100%)`,
+                    height: `calc(${endTimeDec - startTimeDec} / 24 * 100%)`
+                };
+            }
+
+            return styles;
         }
     },
 });
@@ -165,5 +279,19 @@ export default Vue.extend({
 .calendar .schedule .day {
     box-sizing: border-box;
     border-left: 1px solid rgba(0, 0, 0, 0.12);
+}
+
+/* Events */
+
+.calendar .schedule .event {
+    position: absolute;
+
+    width: calc(100% / 8);
+}
+
+.calendar .schedule .event.striped {
+    background: repeating-linear-gradient(
+        -45deg, #f5f3f2c2, #f5f3f2c2 5px, #dfdcdbc2 5px, #dfdcdbc2 10px
+    );
 }
 </style>
