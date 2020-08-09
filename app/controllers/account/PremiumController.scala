01fa984ad056d49fd1fe27269638e3ec6e93bfa6
Raphael Javaux <raphaeljavaux@gmail.com>
2020-08-09 02:04:16 +0200

    Add premium quote form.

---
+++ app/controllers/account/PremiumController.scala
@@ -0,0 +1,36 @@
+/* Noisycamp is a platform for booking music studios.
+ * Copyright (C) 2020  Raphael Javaux <raphael@noisycamp.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package controllers.account
+
+import javax.inject._
+
+import play.api.mvc._
+
+import _root_.controllers.{ CustomBaseController, CustomControllerCompoments }
+import forms.account.PremiumForm
+
+@Singleton
+class PremiumController @Inject() (ccc: CustomControllerCompoments)
+    extends CustomBaseController(ccc) {
+
+    import profile.api._
+
+    def upgrade = silhouette.SecuredAction { implicit request =>
+        Ok(views.html.account.premium.upgrade(request.identity, PremiumForm.form))
+    }
+}
---
+++ app/forms/account/PremiumForm.scala
@@ -0,0 +1,55 @@
+/* Noisycamp is a platform for booking music studios.
+ * Copyright (C) 2020  Raphael Javaux <raphael@noisycamp.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package forms.account
+
+
+import play.api.data.Form
+import play.api.data.Forms._
+
+import forms.CustomFields
+
+/** A form to request access to NoisyCamp Premium. */
+object PremiumForm {
+
+    val form = Form(
+        mapping(
+            "nonprofit"                 -> boolean,
+            "website"                   -> optional(nonEmptyText),
+
+            "rooms"                     -> number,
+
+            "lower-transaction-rate"    -> boolean,
+            "daily-payouts"             -> boolean,
+            "calendar-sync"             -> boolean,
+            "website-integration"       -> boolean,
+            "manual-bookings"           -> boolean
+        )(Data.apply)(Data.unapply))
+
+    case class Data(
+        nonprofit:              Boolean,
+        website:                Option[String],
+
+        rooms:                  Int,
+
+        // Requested features
+        lowerTransactionRate:   Boolean,
+        dailyPayouts:           Boolean,
+        calendarSync:           Boolean,
+        websiteIntegration:     Boolean,
+        manualBookings:         Boolean)
+}
