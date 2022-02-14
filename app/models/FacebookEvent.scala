0fc38b836d0791082d6c5f80e934e77f6d34c063
Raphael Javaux <raphaeljavaux@gmail.com>
2022-02-14 13:12:20 +0100

    Adds models for Facebook event properties.

--- app/models/FacebookEvent.scala
+++ app/models/FacebookEvent.scala
@@ -1,6 +1,7 @@
 package models
 
-import play.api.libs.json.{ JsObject, JsValue }
+import play.api.libs.json.{ JsArray, JsBoolean, JsNumber, Json, JsString, JsValue }
+import squants.market
 
 object FacebookEventName extends Enumeration {
     case class Val(val name: String)
@@ -24,6 +25,97 @@ object FacebookEventName extends Enumeration {
     val ViewContent = Val("ViewContent")
 }
 
+sealed trait FacebookEventProperty {
+    /** Returns the property's value as a (key, value) pair. */
+    def asJson: (String, JsValue)
+}
+
+final case class FacebookContentCategory(val value: String) extends FacebookEventProperty {
+    def asJson = "content_category" -> JsString(value)
+}
+
+final case class FacebookContentIds(val values: Seq[String]) extends FacebookEventProperty {
+    def asJson = "content_ids" -> JsArray(values.map(JsString(_)))
+}
+
+final case class FacebookContentName(val value: String) extends FacebookEventProperty {
+    def asJson = "content_name" -> JsString(value)
+}
+
+object FacebookContentTypeValues extends Enumeration {
+    case class Val(val name: String)
+
+    val Product = Val("product")
+    val ProductGroup = Val("product_group")
+}
+
+final case class FacebookContentType(val value: FacebookContentTypeValues.Val)
+    extends FacebookEventProperty {
+
+    def asJson = "content_type" -> JsString(value.name)
+}
+
+case class FacebookContentValue(
+    val id: String,
+    val quantity: Int,
+)
+
+final case class FacebookContent(val values: Seq[FacebookContentValue])
+    extends FacebookEventProperty {
+
+    def asJson = {
+        val jsonValues = values.map { v => Json.obj("id" -> v.id, "quantity" -> v.quantity) }
+        "contents" -> JsArray(jsonValues)
+    }
+}
+
+final case class FacebookCurrency(val value: market.Currency)
+    extends FacebookEventProperty {
+
+    def asJson = "currency" -> JsString(value.code)
+}
+
+object FacebookDeliveryCategoryValue extends Enumeration {
+    case class Val(val name: String)
+
+    val InStore = Val("in_store")
+    val Curbside = Val("curbside")
+    val HomeDelivery = Val("home_delivery")
+}
+
+final case class FacebookDeliveryCategory(val value: FacebookDeliveryCategoryValue.Val)
+    extends FacebookEventProperty {
+
+    def asJson = "delivery_category" -> JsString(value.name)
+}
+
+final case class FacebookNumItems(val value: Int)
+    extends FacebookEventProperty {
+
+    def asJson = "num_items" -> JsNumber(value)
+}
+
+final case class FacebookPredictedLtv(val value: BigDecimal) extends FacebookEventProperty {
+
+    def asJson = "predicted_ltv" -> JsNumber(value)
+}
+
+final case class FacebookSearchString(val value: String) extends FacebookEventProperty {
+
+    def asJson = "search_string" -> JsString(value)
+}
+
+final case class FacebookStatus(val value: Boolean) extends FacebookEventProperty {
+
+    def asJson = "status" -> JsBoolean(value)
+}
+
+final case class FacebookValue(val value: BigDecimal) extends FacebookEventProperty {
+
+    def asJson = "value" -> JsNumber(value)
+}
+
+
 case class FacebookEvent(
     val name: FacebookEventName.Val,
-    val properties: Option[JsObject] = None)
+    val properties: Seq[FacebookEventProperty] = Seq.empty)
